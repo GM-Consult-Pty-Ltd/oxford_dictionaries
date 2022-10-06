d0cc803c6379a3f42141fdc74cc1643e3d4be556
GerhardMalan <68113701+GerhardMalan@users.noreply.github.com>
2022-10-07 10:10:35 +1100

    Initial commit

---
+++ example/example.dart
@@ -0,0 +1,29 @@
+// Copyright ©2022, GM Consult (Pty) Ltd.
+// BSD 3-Clause License
+// All rights reserved
+
+// ignore_for_file: unused_import
+import 'package:dart_package_template/dart_package_template.dart';
+import 'package:gmconsult_dev/gmconsult_dev.dart';
+
+void main() {
+  // run the README.md example(s)
+  _readMeExample();
+  _seperator();
+  //TODO: example
+  //example goes here
+}
+
+// Simple example(s) for the README.md file.
+Future<void> _readMeExample() async {
+  //
+  // print a heading
+  print('README.md EXAMPLE');
+  //TODO: README.md example
+}
+
+// Print separator
+void _seperator() {
+  print(''.padRight(80, '-'));
+  print('');
+}
